9170627e76291cb9b6855da19ca13e35a6cfeb60
Vinicius Chiele <vinicius.chiele@gmail.com>
2017-03-19 23:06:41 +0000

    Add new example

--- examples/config_per_environment2/main.py
+++ examples/config_per_environment2/main.py
@@ -1,17 +1,11 @@
-from central.config import FileConfig, MergeConfig
+from central.config import FileConfig
 
-
-config = MergeConfig([
-    FileConfig('config.json'),
-    FileConfig('config.${APP_ENV}.json')
-])
-
+config = FileConfig('config.json')
 config.load()
 
 print(config.get('timeout'))
 print(config.get('database'))
 
 # you can set APP_ENV by using environment variables
-
 # export APP_ENV=prod
 # python main.py
