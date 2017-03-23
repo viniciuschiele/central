bc02f636457694cd118fb7d51e1ea6356a1ccaed
Vinicius Chiele <vinicius.chiele@gmail.com>
2017-03-23 17:29:26 +0000

    Add support for automatically detection of changes

--- central/config/etcd.py
+++ central/config/etcd.py
@@ -2,6 +2,9 @@
 etcd config implementation.
 """
 
+import logging
+
+from threading import Event, Thread
 from ..compat import string_types
 from ..exceptions import LibraryRequiredError
 from ..structures import IgnoreCaseDict
@@ -18,12 +21,15 @@ __all__ = [
 ]
 
 
+logger = logging.getLogger(__name__)
+
+
 class EtcdConfig(BaseDataConfig):
     """
     A etcd configuration based on `BaseDataConfig`.
 
     This implementation requires the path to the etcd directory that contains
-    nodes that represent each configuration property. Sub nodes are not read.
+    nodes that represent each configuration property.
 
     An example etcd configuration path is /appname/config
     An example etcd property node path is /appname/config/database.host
@@ -65,6 +71,9 @@ class EtcdConfig(BaseDataConfig):
 
         self._client = client
         self._path = path
+        self._etcd_index = None
+        self._watching = False
+        self._closed = Event()
 
     @property
     def client(self):
@@ -82,25 +91,93 @@ class EtcdConfig(BaseDataConfig):
         """
         return self._path
 
+    @property
+    def path(self):
+        """
+        Get the path to the etcd directory that contains nodes.
+        :return str: The path to the etcd directory that contains nodes.
+        """
+        return self._path
+
+    def close(self):
+        """
+        Stop watching the path for changes.
+        """
+        self._closed.set()
+
     def load(self):
         """
         Read the configuration stored in the etcd.
         """
-        result = self._client.read(self._path)
-
         data = IgnoreCaseDict()
 
+        result = self._client.read(self._path, recursive=True)
+
         for item in result.leaves:
-            # sub nodes are ignored
+            # directories are ignored
             if item.dir:
                 continue
 
             # strips off the path from the key.
-            index = item.key.rfind('/') + 1
-
-            key = item.key[index:].replace('/', '.')
+            key = self._transform_key(item.key)
             value = item.value
 
             data[key] = value
 
+        self._etcd_index = result.etcd_index + 1
         self._data = data
+
+        if not self._watching:
+            thread = Thread(target=self._watch, name='EtcdConfig')
+            thread.daemon = True
+            thread.start()
+
+            self._watching = True
+
+    def _transform_key(self, key):
+        """
+        Strip off the from the key.
+        :param str key: The key from etcd.
+        :return str: The key without the base path.
+        """
+        return key.lstrip(self._path).lstrip('/').replace('/', '.')
+
+    def _watch(self):
+        """
+        Watch the path for changes.
+        """
+        while not self._closed.is_set():
+            try:
+                result = self._client.watch(self._path, index=self._etcd_index, recursive=True)
+
+                for item in result.leaves:
+                    # directories are ignored
+                    if item.dir:
+                        continue
+
+                    key = self._transform_key(item.key)
+                    value = item.value
+
+                    if item.action == 'create':
+                        self._data[key] = value
+
+                    elif item.action == 'set':
+                        self._data[key] = value
+
+                    elif item.action == 'delete':
+                        self._data.pop(key, None)
+
+                    else:
+                        logger.warning('Unrecognized etcd action %s ', item.action)
+
+                self._etcd_index = result.modifiedIndex + 1
+
+                try:
+                    self.updated()
+                except:
+                    logger.warning('Error calling updated event from ' + str(self), exc_info=True)
+
+            except etcd.EtcdWatchTimedOut:
+                pass
+            except:
+                logger.exception('Etcd watch for path %s failed' % self._path)
